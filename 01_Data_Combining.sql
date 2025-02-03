abfd53a65120ef75c3dc07db4478c8e7e8610771
tarekab-87 <tarek_esmaeel@hotmail.com>
2025-02-03 17:37:55 +0100

    update data combining

--- 01_Data_Combining.sql
+++ 01_Data_Combining.sql
@@ -1,4 +1,5 @@
 -- Active: 1735587012946@@127.0.0.1@5432@case_study_2@public
+-- merge the the tables daily_activity_1 and daily_activity_2
 CREATE TABLE public.daily_activity  AS 
 SELECT * FROM public.daily_activity_1
 WHERE FALSE;
@@ -8,8 +9,30 @@ SELECT * FROM daily_activity_1
 UNION ALL
 SELECT * FROM daily_activity_2
 
-SELECT
-    *
-FROM 
-    daily_activity
+-- merge the 2 tables minute_sleep_1_v1 and minute_sleep_2_v1
+CREATE table public.minute_sleep AS
+SELECT * FROM minute_sleep_1_v1
+WHERE FALSE;
 
+INSERT INTO minute_sleep
+SELECT * FROM minute_sleep_1_v1
+UNION ALL
+SELECT * FROM minute_sleep_2_v1
+
+-- merge the 2 tables hourly_steps_1 and hourly_steps_2
+CREATE TABLE hourly_steps AS
+(
+    SELECT 
+        * 
+    FROM 
+        hourly_steps_1
+    WHERE
+        FALSE
+);
+
+INSERT INTO hourly_steps
+SELECT * FROM hourly_steps_1
+UNION ALL
+SELECT * FROM hourly_steps_2
+
+SELECT * FROM hourly_steps
--- 02_Data_Exploration.sql
+++ 02_Data_Exploration.sql
@@ -1,13 +1,26 @@
--- Active: 1735587012946@@127.0.0.1@5432@case_study_2@public
-SELECT
-    *
-FROM 
-    daily_activity
 
+-- explore daily activity table 
+
+SELECT * FROM daily_activity
 
 SELECT
-    *
+  COUNT(*) - COUNT('Id') AS id_null,
+  COUNT(*) - COUNT('ActivityDate') AS activity_date_null_count,
+  COUNT(*) - COUNT('TotalSteps') AS total_steps_null_count,
+  COUNT(*) - COUNT('TotalDistance') AS total_distance_null_count,
+  COUNT(*) - COUNT('TrackerDistance') AS tracker_distance_null_count,
+  COUNT(*) - COUNT('LoggedActivitiesDistance') AS logged_distance_null_count,
+  COUNT(*) - COUNT('VeryActiveDistance') AS very_avtive_distance_null_count,
+  COUNT(*) - COUNT('ModeratelyActiveDistance') AS moderate_active_distance_null_count,
+  COUNT(*) - COUNT('LightActiveDistance') AS light_active_distance_null_count,
+  COUNT(*) - COUNT('SedentaryActiveDistance') AS sedantary_active_distance_null_count,
+  COUNT(*) - COUNT('VeryActiveMinutes') AS very_active_minute_null_count,
+  COUNT(*) - COUNT('FairlyActiveMinutes') AS fairly_active_minutes_null_count,
+  COUNT(*) - COUNT('LightlyActiveMinutes') AS lightly_active_minutes_null_count,
+  COUNT(*) - COUNT('SedentaryMinutes') AS sedentary_minutes_null_count,
+  COUNT(*) - COUNT('Calories') AS calories_null_count
 FROM
     daily_activity
-WHERE
-    "Id" = '1503960366'
+
+-- no null values were found
+
